b429dc0ef60999b628e5cdc8961f5fa9cd73e9db
joazlz <48420841+joazlz@users.noreply.github.com>
2023-03-05 12:36:06 -0600

    Initial commit

---
+++ src/main/webui/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+
+let info:string = "";
+
+axios
+  .get('http://localhost:8080/hello')
+  .then(response => {
+    info = response.data.bpi
+  });
+</script>
+
+<template>
+  <h1>{{ info }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      <code>HOLA ERICK</code>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
